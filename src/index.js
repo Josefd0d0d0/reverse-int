a6aeed704c9f55504c3d06e47278e537463532b6
Josefd0d0d0 <119966356+Josefd0d0d0@users.noreply.github.com>
2023-02-28 00:35:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+  let reversedNum = '';
+    let numString = n.toString();
   
+    for (let i = numString.length - 1; i >= 0; i--) {
+      reversedNum += numString[i];
+    }
+  
+    return parseInt(reversedNum)
 }
